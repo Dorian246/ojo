8e8aa49c08be532463bc79d80b602196b1c8dad2
Dorian De Jesus Zuñiga <106410003+Dorian246@users.noreply.github.com>
2023-08-13 22:08:15 -0600

    commit-archivos

---
+++ ojo.js
@@ -0,0 +1,9 @@
+let punto = document.querySelector(".punto");
+let ojo = document.querySelector(".ojo");
+
+document.onmousemove = (e) => {
+    let x = e.clientX * 100 / window.innerWidth + "%";
+     let y = e.clientY * 100 / window.innerHeight + "%";
+     punto.style.left= x;
+       punto.style.top= y;
+}
